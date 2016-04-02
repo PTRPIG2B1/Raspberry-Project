40d03e948fa38c2bfbaa3289f2d13b9c94be5037
PTRPIG2B1 <PTRPIG2B1@users.noreply.github.com>
2016-04-02 12:35:41 +0200

    Comparaison de deux images avec un seuil de tolérance

--- developpement/testcomparaison.py
+++ developpement/testcomparaison.py
@@ -3,37 +3,75 @@ from PIL import Image
 
 #Note impossible de faire deux imports
 #Impossible de faire new ET getpixel
-
-SEUIL = 20
+constSeuil = 1000
+Seuil = 0.5
 res1 = 0
 res2 = 0
+vert = 0
+rouge = 0
+bleu = 0
 width = 640
 height = 480
 
-image1 = Image.new("RGB", (width,height), "white")
-image2 = Image.new("RGB", (width,height), "red")
+try:
+  image1 = Image.open("image1.png")
+except IOError:
+  print 'Erreur sur ouverture du fichier ' 
+  sys.exit(1)
 
-image1.save("image1.png", format="png")
-image2.save("image2.png", format="png")
+try:
+  image2 = Image.open("image2.png")
+except IOError:
+  print 'Erreur sur ouverture du fichier ' 
+  sys.exit(1)
 
-data1 = image1.getdata()
-type(data1)
+rgb_im1 = image1.convert('RGB')
+rgb_im2 = image2.convert('RGB')
 
-data2 = image2.getdata()
-type(data2)
 
-l1 = list(data1)
-l2 = list(data2)
 
-for i in range (0,9):
+
+for i in range (0,10):
   ligne = i*width/10
-  for j in range (0,9):
+  for j in range (0,10):
     colonne = j*height/10
+    zoneVert1=0
+    zoneRouge1=0
+    zoneBleu1=0
+    zoneVert2=0
+    zoneBleu2=0
+    zoneRouge2=0
     for k in range (0,63):
       for l in range(0,47):
-        (rouge1,vert1,bleu1) = image1.getpixel((k+ligne,l+colonne))
-        (rouge2,vert2,bleu2) = image2.getpixel((k+ligne,l+colonne))
-        res1 = res1 + rouge1 + vert1 + bleu1
-        res2 = res2 + rouge2 + vert2 + bleu2
-res = abs(res2 - res1)
-print(res)
+        (rouge1,vert1,bleu1) = rgb_im1.getpixel((k+ligne,l+colonne))
+        (rouge2,vert2,bleu2) = rgb_im2.getpixel((k+ligne,l+colonne))
+        zoneRouge1=zoneRouge1+rouge1
+        zoneRouge2=zoneRouge2+rouge2
+        zoneVert1=zoneVert1+vert1
+        zoneVert2=zoneVert2+vert2
+        zoneBleu1=zoneBleu1+bleu1
+        zoneBleu2=zoneBleu2+bleu2
+    if(zoneVert2<=zoneVert1+zoneVert1*(Seuil) and zoneVert2>=zoneVert1-zoneVert1*(Seuil)):
+      vert = 0
+    else:
+      vert = abs(zoneVert2-zoneVert1)
+
+    if(zoneRouge2<=zoneRouge1+zoneRouge1*(Seuil) and zoneRouge2>=zoneRouge1-zoneRouge1*(Seuil)):
+      rouge= 0
+    else:
+      rouge= abs(zoneRouge2-zoneRouge1)
+
+    if(zoneBleu2<=zoneBleu1+zoneBleu1*(Seuil) and zoneBleu2>=zoneBleu1-zoneBleu1*(Seuil)):
+      bleu = 0
+    else:
+      bleu = abs(zoneBleu2-zoneBleu1)
+				
+    res = vert+bleu+rouge 
+    
+    if res == 0:
+      print "pas de mouv"
+    else:
+      print "mouv"
+      
+
+
