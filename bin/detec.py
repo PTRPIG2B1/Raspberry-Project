6c321589712debb682dcecb2970d05f4044e40df
PTRPIG2B1 <PTRPIG2B1@users.noreply.github.com>
2016-04-07 12:20:40 +0200

    MAJ, detec = version conor

--- bin/detec.py
+++ bin/detec.py
@@ -1,35 +1,64 @@
-#Les importations 
-import os
-import time
-import ConfigParser
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# imports
 from PIL import Image
+import math
+import sys
+import time
+import os
+import ConfigParser
 
-#Declaration des variables
-#enFonctionnement = True
+# Declaration CONSTANTES / variables
+difference = 0
+WIDTH = 640
+HEIGHT = 480
+SLEEPTIME = 1
 
-#Debut du programme
+SEUILMIN = 100000
+SEUILMAX = 2000000
+SEUILECART = SEUILMAX - SEUILMIN
+
 
 cfg = ConfigParser.ConfigParser()
 cfg.read('config.cfg')
 
 enFonctionnement = cfg.get('Section1', 'detectEnMarche')
 
-#Rajouter la gestion des erreurs (probleme camera)
-#im = Image.new("RGB", (500,500), "red")
-#im.save("imagecree.png", format="png")
-while enFonctionnement == 'True' :
-	os.system('raspistill -o photo1.jpg -t 500')
-	while enFonctionnement == 'True' : 
-		os.system('raspistill -o photo2.jpg -t 500')
-		image1 = Image.open("photo1.jpg")
-		image2 = Image.open("photo2.jpg")
-		cfg.read('config.cfg')
-		enFonctionnement = cfg.get('Section1', 'detectEnMarche')
-		os.system('mv photo1.jpg temp')
-		os.system('mv photo2.jpg photo1.jpg')
-	os.system('rm temp')  
-	os.system('rm photo1.jpg')
-      
+SEUILCONF = cfg.get('Section1', 'seuil')
+SEUIL = (int(SEUILCONF)/100*SEUILECART + SEUILMIN)
+
+os.system('raspistill -w '+str(WIDTH)+' -h '+str(HEIGHT)+' -t 500 -o image1.jpg')
+os.system('raspistill -w '+str(WIDTH)+' -h '+str(HEIGHT)+' -t 500 -o image2.jpg')
+
+while enFonctionnement == 'True' : 
+    image1 = Image.open("image1.jpg")
+    image2 = Image.open("image2.jpg")
+    rgb_im1 = image1.convert('RGB')
+    rgb_im2 = image2.convert('RGB')
+    
+    for i in range (0,10):
+      ligne = i*WIDTH/10
+      for j in range (0,10):
+        colonne = j*HEIGHT/10
+        difference = 0
+        for k in range (0,(WIDTH/10-1)):
+          for l in range(0,(HEIGHT/10-1)):
+            (rouge1,vert1,bleu1) = rgb_im1.getpixel((k+ligne,l+colonne))
+            (rouge2,vert2,bleu2) = rgb_im2.getpixel((k+ligne,l+colonne))
+            difference += math.fabs(rouge1-rouge2)+math.fabs(vert1-vert1)+math.fabs(bleu1-bleu2)
+
+    
+        #print "Zone ("+str(i)+","+str(j)+") : "+str(difference)
+    
+    os.system('mv image2.jpg image1.jpg')
+    os.system('raspistill -w '+str(WIDTH)+' -h '+str(HEIGHT)+' -t 500 -o image2.jpg')
+    
+    cfg.read('config.cfg')
+    enFonctionnement = cfg.get('Section1', 'detectEnMarche')
+    time.sleep(SLEEPTIME)
+
+os.system('rm image1.jpg && rm image2.jpg')
 
 
 
