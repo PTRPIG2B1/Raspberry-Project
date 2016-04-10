8d41081ab988f452b780d4c9db256b03846bcf6d
ConorRyan <conor.ryan@etu.unilim.fr>
2016-04-10 17:46:48 +0200

    Debut de refactoring, utilisez maintenant les fonctions de actions.py

--- bin/detec.py
+++ bin/detec.py
@@ -10,48 +10,41 @@ import os
 import ConfigParser
 
 # Declaration CONSTANTES / variables
-difference = 0
-WIDTH = 640
-HEIGHT = 480
-SLEEPTIME = 1
-SEUILMIN = 100000
-SEUILMAX = 2000000
-SEUILECART = SEUILMAX - SEUILMIN
+LARGEUR = 640
+HAUTEUR = 480
+SEUIL_MIN = 100000
+SEUIL_MAX = 2000000
+SEUIL_ECART = SEUIL_MAX - SEUIL_MIN
 
-CONFIGPATH = '/home/pi/RaspiWatch/bin/config.cfg'
-SECTIONVIDEO ='Section1'
-IMAGE1PATH='/home/pi/RaspiWatch/bin/image1.jpg'
-IMAGE2PATH='/home/pi/RaspiWatch/bin/image2.jpg'
+CONFIG_PATH = '/home/pi/RaspiWatch/bin/config.cfg'
+IMAGE1_PATH='/home/pi/RaspiWatch/bin/image1.jpg'
+IMAGE2_PATH='/home/pi/RaspiWatch/bin/image2.jpg'
 
 cfg = ConfigParser.ConfigParser()
-cfg.read(CONFIGPATH)
-cfg.set(SECTIONVIDEO,'detectenmarche','True')
-cfg.write(open(CONFIGPATH, 'w'))
+cfg.read(CONFIG_PATH)
 
-enFonctionnement = cfg.get(SECTIONVIDEO, 'detectenmarche')
+SEUIL_CONF = cfg.get('Detection', 'seuil')
+SEUIL = (int(SEUIL_CONF)/100*SEUIL_ECART + SEUIL_MIN)
 
+os.system('raspistill -w '+str(LARGEUR)+' -h '+str(HAUTEUR)+' -t 500 -o '+IMAGE1_PATH)
+os.system('raspistill -w '+str(LARGEUR)+' -h '+str(HAUTEUR)+' -t 500 -o '+IMAGE2_PATH)
 
-SEUILCONF = cfg.get(SECTIONVIDEO, 'seuil')
-SEUIL = (int(SEUILCONF)/100*SEUILECART + SEUILMIN)
-
-os.system('raspistill -w '+str(WIDTH)+' -h '+str(HEIGHT)+' -t 500 -o '+IMAGE1PATH)
-os.system('raspistill -w '+str(WIDTH)+' -h '+str(HEIGHT)+' -t 500 -o '+IMAGE2PATH)
-
+enFonctionnement = True
 while enFonctionnement == 'True' : 
-    image1 = Image.open(IMAGE1PATH)
-    image2 = Image.open(IMAGE2PATH)
+    image1 = Image.open(IMAGE1_PATH)
+    image2 = Image.open(IMAGE2_PATH)
     rgb_im1 = image1.convert('RGB')
     rgb_im2 = image2.convert('RGB')
     
     nbMouvement = 0
     
     for i in range (0,10):
-      ligne = i*WIDTH/10
+      ligne = i*LARGEUR/10
       for j in range (0,10):
-        colonne = j*HEIGHT/10
+        colonne = j*HAUTEUR/10
         difference = 0
-	for k in range(0, (WIDTH/10-1)/3):	
-          for l in range(0,(HEIGHT/10-1)/3):
+	for k in range(0, (LARGEUR/10-1)/3):	
+          for l in range(0,(HAUTEUR/10-1)/3):
             (rouge1,vert1,bleu1) = rgb_im1.getpixel((k+ligne,l+colonne))
             (rouge2,vert2,bleu2) = rgb_im2.getpixel((k+ligne,l+colonne))
             difference += math.fabs(rouge1-rouge2)+math.fabs(vert1-vert1)+math.fabs(bleu1-bleu2)
@@ -63,15 +56,11 @@ while enFonctionnement == 'True' :
     if nbMouvement > 20 :
         print "Mouvement"
         
-    os.system('mv '+IMAGE2PATH+' '+IMAGE1PATH)
-    os.system('raspistill -w '+str(WIDTH)+' -h '+str(HEIGHT)+' -t 500 -o '+IMAGE2PATH)
+    os.system('mv '+IMAGE2_PATH+' '+IMAGE1_PATH)
+    os.system('raspistill -w '+str(LARGEUR)+' -h '+str(HAUTEUR)+' -t 500 -o '+IMAGE2_PATH)
     
-    cfg.read(CONFIGPATH)
-    enFonctionnement = cfg.get(SECTIONVIDEO, 'detectenmarche')
-    #time.sleep(SLEEPTIME)
+    cfg.read(CONFIG_PATH)
+    enFonctionnement = cfg.get('Detection', 'enmarche')
 
-os.system('rm '+IMAGE1PATH+' && rm '+IMAGE2PATH)
+os.system('rm '+IMAGE1_PATH+' && rm '+IMAGE2_PATH)
 
-
-
-#Fonction decomparaison retourne un booleen (true si mouvement)
--- bin/raspiwatch.py
+++ bin/raspiwatch.py
@@ -1,7 +1,11 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
 import os
 import ConfigParser
 import sys
 from datetime import datetime
+
 #Fonctions et procedure
 def affmenuprincipalActif():
 	print '////////////////MENU PRINCIPAL RASPBERRY////////////////\n'
