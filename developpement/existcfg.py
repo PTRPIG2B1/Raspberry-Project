b9e714f989837018f7454a7f9802791067f58b72
PTRPIG2B1 <PTRPIG2B1@users.noreply.github.com>
2016-06-15 13:04:57 +0200

    Suppression fichiers inutiles

--- developpement/existcfg.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/python2
-import os
-if not os.path.isfile('config.cfg'):
-  print 'Creation'
-  os.system("python makeconfig.py")
-else : 
-  print 'Le fichier existe'
--- developpement/testLancement.py
+++
@@ -1,8 +0,0 @@
-import picamera
-import time
-camera = picamera.PiCamera()
-
-camera.start_recording('video.h264')
-time.sleep(5)
-
-camera.stop_recording()
